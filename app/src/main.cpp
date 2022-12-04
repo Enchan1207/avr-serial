468cad98a071c8c40f452051958f190bcc7c34d2
Enchan <enchant0130@gmail.com>
2022-12-04 18:07:33 +0900

    [Update #3] USART echo-back

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -8,6 +8,11 @@
 
 #define BAUDRATE 115200
 #define LED_BUILTIN 5  // Dev board has Built-in LED @ PORTB:5
+#define bit_set(v, n) v |= _BV(n)
+#define bit_reset(v, n) v &= ~_BV(n)
+
+// 最後に受信した値
+int16_t* LastChar;
 
 /**
  * @brief initialize Serial communication interface (UART).
@@ -19,22 +24,47 @@ void initSerial(const uint64_t& baudrate) {
     const uint64_t ubrr_value = (uint64_t)F_CPU / 16 / (baudrate - 1);
     UBRR0 = (uint16_t)ubrr_value;
 
-    // allow to send
-    UCSR0B = _BV(TXEN0);
+    // enable interrupt
+    UCSR0B |= _BV(TXCIE0) | _BV(RXCIE0) | _BV(UDRE0);
+
+    // allow to send/recv
+    UCSR0B |= _BV(TXEN0) | _BV(RXEN0);
 }
 
 /**
  * 送信バッファ空き
  */
 ISR(USART_UDRE_vect) {
-    // latch built-in led
-    PORTB &= ~_BV(LED_BUILTIN);
+    // 送信するべきデータがなければ戻る
+    if (*LastChar == -1) {
+        return;
+    }
+
+    // データバッファに書き出す
+    UDR0 = (char)*LastChar;
+    *LastChar = -1;
+
+    bit_reset(PORTB, LED_BUILTIN);
 }
 
-void main() {
-    // Initialize GPIO
-    DDRB |= _BV(LED_BUILTIN);
-    PORTB |= _BV(LED_BUILTIN);
+/**
+ * 受信完了
+ */
+ISR(USART_RX_vect) {
+    bit_set(PORTB, LED_BUILTIN);
+
+    // データバッファに取り込む
+    *LastChar = UDR0;
+}
+
+int main() {
+    // initialize GPIO
+    bit_set(DDRB, LED_BUILTIN);
+    bit_reset(PORTB, LED_BUILTIN);
+
+    // initialize USART buffer
+    int16_t lastChar = -1;
+    LastChar = &lastChar;
 
     // enable interrupt
     sei();
